ecee656e214ed2e40419fe13f726d2744952ea07
Aniket Chauhan <57026803+Aniket-68@users.noreply.github.com>
2026-07-14 23:53:31 +0000

    Sync LeetCode submission - Valid Anagram (python3)

---
+++ leetcode-solutions/problems/valid_anagram/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+
+        # return Counter(s) == Counter(t) #nlogn
+        
+        # return sorted(s) == sorted(t) # nlogn
+
+
+        # HashMap Approach
+        freq={}
+        for ch in s:
+        #   if ch not in t:
+        #     return False # O(n) --> Since List, String,tuple
+          freq[ch]=freq.get(ch,0)+1 
+
+
+        for ch in t:
+            if ch not in freq: # O(1) Lookup since dict, set
+                return False
+            freq[ch]-=1
+            
+            if freq[ch] <0:
+                return False
+        return True
+
+            
+        
+             
+        
+        
